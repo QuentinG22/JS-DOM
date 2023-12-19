15ef5d4322d2329d7874103ec83d35d5c12a6703
QuentinG22 <quentin-guillemin@orange.fr>
2023-12-19 18:30:06 +0100

    Exercice 9 et 10 effectuer

--- images.js
+++ images.js
@@ -1,6 +1,41 @@
 // 1. Récupérer les éléments HTML des 2 boutons (left et right)
+let previous = document.querySelector('.left');
+let next = document.querySelector('.right');
 
 // 2. Ajouter des écouteurs d'événement à ces 2 boutons
+next.addEventListener('click', nextSlide);
+previous.addEventListener('click', previousSlide);
 
 // 3. Ecrire les fonctions de rappel (callback)
+let slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
 
+// for(let i = 0; i < slides.length; i++){
+//     if(slides[i].className.includes('active')){
+//         currentIndex = i
+//     }
+// }
+
+slides.forEach((slide, i) => {
+    if(slide.className.includes('active')){
+        currentIndex = i
+    }
+})
+
+function previousSlide(){
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+
+function nextSlide(){
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+function showSlide(index) {
+    slides.forEach((slide) => {
+        slide.classList.add('hidden');
+    });
+
+    slides[index].classList.remove('hidden');
+}
--- listen.js
+++ listen.js
@@ -1,1 +1,9 @@
 // CODE HERE
+let button = document.querySelector('button')
+
+function setMessage(){
+    let para = document.querySelector('p');
+    para.classList.remove('hidden');
+}
+
+button.addEventListener('click', setMessage);
